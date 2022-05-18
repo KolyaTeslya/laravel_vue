223897a5b0b002c2e211ed709de598579e8cfbad
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-05-18 20:56:11 +0300

    creating components and embedding one component into another

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <SinglePostComponent></SinglePostComponent>
+    </div>
+</template>
+
+<script>
+import SinglePostComponent from "./SinglePostComponent";
+export default {
+    name: "PostComponent",
+
+    data: function () {
+        return{
+            name: 'Vasya',
+            age: 20,
+            boolean: false,
+            someNull: null,
+            arr: [
+
+            ]
+
+        }
+    },
+
+    components: {
+        SinglePostComponent
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
